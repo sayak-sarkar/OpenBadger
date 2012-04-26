e2faee10d7a4f8fc4b0bae6f28912050c50a9029
Brian J Brennan <brianloveswords@gmail.com>
2012-04-26 16:47:50 -0400

    Add some more information to the model REPL.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,15 @@
 if (!module.parent) {
+  var colors = require('colors');
   var fs = require('fs');
   var files = fs.readdirSync(__dirname)
     .filter(function (f) { return f !== 'index.js' });
 
+  console.log("starting REPL with the following models in the global namespace:");
+  
   files.forEach(function (f) {
     var name = f.charAt(0).toUpperCase() + f.replace('.js', '').substring(1);
     root[name] = require('./' + f);
+    console.log("* ".green + name);
   });
   
   var repl = require('repl').start("models> ");
